1bdaeaa4fa300b6cf050eaa54d0f18d837719be4
Jambor Yao <jianbyao@microsoft.com>
2018-08-02 11:08:38 +0800

    Add project files.

---
+++ PhoneHandler/PhoneHandler.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace PhoneHandler
+{
+    public class PhoneHttpHandler : IHttpHandler
+    {
+        public bool IsReusable => true;
+
+        public void ProcessRequest(HttpContext context)
+        {
+            context.Response.Write("This is a phone number!");
+        }
+    }
+}
